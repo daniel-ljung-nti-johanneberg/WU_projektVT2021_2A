5d4c9a72ba6e9de0ff29733742dd501ac85743cf
daniel-ljung-nti-johanneberg <74712537+daniel-ljung-nti-johanneberg@users.noreply.github.com>
2021-03-08 10:57:27 +0100

    lagt till hamburger-meny

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".meny-toggle")
+const nav = document.querySelector(".nav-links")
+
+function toggleMeny() {
+    burger.classList.toggle("toggle")
+    nav.classList.toggle("nav-active")
+}
+
+burger.addEventListener("click", toggleMeny)
