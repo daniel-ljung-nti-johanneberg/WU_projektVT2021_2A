65971aa6d00ac0431f15dae1a7b44c011101a08e
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2021-04-02 21:23:10 +0200

    presentkorts-sida, justering på t-shirt sida, fler tshirts

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const burger = document.querySelector(".meny-toggle")
 const nav = document.querySelector(".nav-links")
 
+
+
 function toggleMeny() {
     burger.classList.toggle("toggle")
     nav.classList.toggle("nav-active")
 }
+
 
 burger.addEventListener("click", toggleMeny)
